d6fad612a9ca759bc005d0d7320025f88a5000bd
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-06 12:23:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,95 +4,99 @@ from client import RestClient
 import pandas as pd
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
+st.title("Trustpilot reviews")
+domain = st.text_input('Domain', 'www.ashleystewart.com')
 
-#POST
-post_data = dict()
-# simple way to set a task
-post_data[len(post_data)] = dict(
-    domain="www.ashleystewart.com"
-)
-# after a task is completed, we will send a GET request to the address you specify
-# instead of $id and $tag, you will receive actual values that are relevant to this task
-post_data[len(post_data)] = dict(
-    domain="www.ashleystewart.com",
-    depth=100,
-    # priority=2,
-    # tag="some_string_123",
-    pingback_url="https://your-server.com/pingscript?id=$id&tag=$tag"
-)
-# after a task is completed, we will send a GET request to the address you specify
-# instead of $id and $tag, you will receive actual values that are relevant to this task
-post_data[len(post_data)] = dict(
-    domain="www.ashleystewart.com",
-    postback_url="https://your-server.com/postbackscript"
-)
-# POST /v3/business_data/trustpilot/reviews/task_post
-response = client.post("/v3/business_data/trustpilot/reviews/task_post", post_data)
-# you can find the full list of the response codes here https://docs.dataforseo.com/v3/appendix/errors
-if response["status_code"] == 20000:
-    print(response)
-    # do something with result
-else:
-    print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
+if st.button('Get data'):
 
-# st.write(response)
+    #POST
+    post_data = dict()
+    # simple way to set a task
+    post_data[len(post_data)] = dict(
+        domain=domain
+    )
+    # after a task is completed, we will send a GET request to the address you specify
+    # instead of $id and $tag, you will receive actual values that are relevant to this task
+    post_data[len(post_data)] = dict(
+        domain=domain,
+        depth=100,
+        # priority=2,
+        # tag="some_string_123",
+        pingback_url="https://your-server.com/pingscript?id=$id&tag=$tag"
+    )
+    # after a task is completed, we will send a GET request to the address you specify
+    # instead of $id and $tag, you will receive actual values that are relevant to this task
+    post_data[len(post_data)] = dict(
+        domain=domain,
+        postback_url="https://your-server.com/postbackscript"
+    )
+    # POST /v3/business_data/trustpilot/reviews/task_post
+    response = client.post("/v3/business_data/trustpilot/reviews/task_post", post_data)
+    # you can find the full list of the response codes here https://docs.dataforseo.com/v3/appendix/errors
+    if response["status_code"] == 20000:
+        print(response)
+        # do something with result
+    else:
+        print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
 
-#GET
-response = client.get("/v3/business_data/trustpilot/reviews/tasks_ready")
-# you can find the full list of the response codes here https://docs.dataforseo.com/v3/appendix/errors
-if response['status_code'] == 20000:
-    results = []
-    tasks = response['tasks']
-    if tasks:  # Check if the list is not empty
-        last_task = tasks[-1]  # Get the last task
-        if last_task['result'] and len(last_task['result']) > 0:
-            for resultTaskInfo in last_task['result']:
-                if resultTaskInfo['id']:
-                    results.append(client.get("/v3/business_data/trustpilot/reviews/task_get/" + resultTaskInfo['id']))
-                    
-    print(results)
-    # do something with result
-else:
-    print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
+    # st.write(response)
 
-
-
-#EXTRACT
-def extract_product_details_from_list(results):
-    all_products = []
-
-    for result in results:
-        tasks = result.get("tasks", [])
-
-        for task in tasks:
-            result_data = task.get("result", {})
-
-            if isinstance(result_data, dict) and "items" in result_data:
-                items = result_data["items"]
-            elif isinstance(result_data, list) and "items" in result_data[0]:
-                items = result_data[0]["items"]
-            else:
-                items = []
-
-            for item in items:
+    #GET
+    response = client.get("/v3/business_data/trustpilot/reviews/tasks_ready")
+    # you can find the full list of the response codes here https://docs.dataforseo.com/v3/appendix/errors
+    if response['status_code'] == 20000:
+        results = []
+        tasks = response['tasks']
+        if tasks:  # Check if the list is not empty
+            last_task = tasks[-1]  # Get the last task
+            if last_task['result'] and len(last_task['result']) > 0:
+                for resultTaskInfo in last_task['result']:
+                    if resultTaskInfo['id']:
+                        results.append(client.get("/v3/business_data/trustpilot/reviews/task_get/" + resultTaskInfo['id']))
+                        
+        print(results)
+        # do something with result
+    else:
+        print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
 
 
 
-                product_info = {
-                        "rating": item["rating"]["value"],
-                        "timestamp": item["timestamp"],
-                        "review_text": item["review_text"]
-                    }
-                all_products.append(product_info)
+    #EXTRACT
+    def extract_product_details_from_list(results):
+        all_products = []
 
-    return all_products
+        for result in results:
+            tasks = result.get("tasks", [])
 
-# Usage
-products = extract_product_details_from_list(results)
-print(products)  # This should print the details of the first product
+            for task in tasks:
+                result_data = task.get("result", {})
+
+                if isinstance(result_data, dict) and "items" in result_data:
+                    items = result_data["items"]
+                elif isinstance(result_data, list) and "items" in result_data[0]:
+                    items = result_data[0]["items"]
+                else:
+                    items = []
+
+                for item in items:
 
 
-df = pd.DataFrame.from_dict(products)
-df
+
+                    product_info = {
+                            "rating": item["rating"]["value"],
+                            "timestamp": item["timestamp"],
+                            "review_text": item["review_text"]
+                        }
+                    all_products.append(product_info)
+
+        return all_products
+
+    # Usage
+    products = extract_product_details_from_list(results)
+    print(products)  # This should print the details of the first product
+
+    st.success("Success!")
+    df = pd.DataFrame.from_dict(products)
+    df
 
 
