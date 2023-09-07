b90afd08508838c78b618f7d4ab87c80a5a6591c
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-07 11:53:43 +0200

    Create AI - numbers.py

---
+++ pages/AI - numbers.py
@@ -0,0 +1,80 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+from langchain import OpenAI, SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+import openai
+
+
+#####################################
+#            FUNCTIONS              #
+#####################################
+@st.cache_data()
+def load_data(url):
+    """
+    load data from url
+    """
+    df = pd.read_csv(url)
+    return df
+
+def prepare_data(df):
+    """
+    lowercase columns
+    """
+    df.columns = [x.replace(' ', '_').lower() for x in df.columns]
+    return df
+
+
+#####################################
+#        LOCALS & CONSTANTS         #
+#####################################
+table_name = 'statesdb'
+uri = "file::memory:?cache=shared"
+
+
+#####################################
+#            HOME PAGE              #
+#####################################
+st.title('Ask AI about Your numbers')
+st.subheader('Upload a file to query')
+
+# read file
+uploaded_file = st.file_uploader("Choose a csv file")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+
+    # api key
+    openai_api_key = "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"
+
+    # user query
+    user_q = st.text_input(
+        "User Query", 
+        help="Enter a question based on the dataset")
+
+    # commit data to sql
+    data = prepare_data(df)
+    conn = sqlite3.connect(uri)
+    data.to_sql(table_name, conn, if_exists='replace', index=False)
+
+    # create db engine
+    eng = create_engine(
+        url='sqlite:///file:memdb1?mode=memory&cache=shared', 
+        poolclass=StaticPool, # single connection for requests
+        creator=lambda: conn)
+    db = SQLDatabase(engine=eng)
+
+    # create open AI conn and db chain
+    if openai_api_key:
+      llm = OpenAI(
+          openai_api_key=openai_api_key, 
+          temperature=0, # creative scale
+          max_tokens=300)
+      db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
+
+    # run query and display result
+    if openai_api_key and user_q:
+        result = db_chain.run(user_q)
+        st.write(result)
