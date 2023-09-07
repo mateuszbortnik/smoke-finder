1dd75d3658b3f0e99026c90231b0f5d0e0f427ed
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-07 12:55:08 +0200

    Create AI - review analysis.py

---
+++ pages/AI - review analysis.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import math
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA, OpenAI
+from langchain.llms import OpenAIChat
+from langchain.document_loaders import DirectoryLoader
+import langchain
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+
+openai_api_key = "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"
+
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+# read file
+uploaded_file = st.file_uploader("Choose a csv file")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+
+loader = langchain.document_loaders.DataFrameLoader(df, 'review_text')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+docsearch = Chroma.from_documents(texts, embeddings)
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever = docsearch.as_retriever())
+query = st.text_input(
+        "User Query", 
+        help="Enter a question about rviews")
+
+qa.run(query)
