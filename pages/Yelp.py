1e86d06c0126aac0e701f3154bde2b8129bffecd
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-11 11:06:54 +0200

    Update Yelp.py

--- pages/Yelp.py
+++ pages/Yelp.py
@@ -15,10 +15,7 @@ if st.button('Get data'):
     # POST request to enqueue a task
     post_data = dict()
 
-    post_data[len(post_data)] = dict(
-    language_name="English",
-    alias=alias,
-    depth=4490)
+    post_data[len(post_data)] = dict(language_name="English", alias=alias, depth=4490)
 
     response = client.post("/v3/business_data/yelp/reviews/task_post", post_data)
     
