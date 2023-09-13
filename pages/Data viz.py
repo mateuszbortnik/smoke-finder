cffc52b67ef513c72049215497e27940cbbf5a4f
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-13 13:00:14 +0200

    Create Data viz.py

---
+++ pages/Data viz.py
@@ -0,0 +1,22 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+ 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+ 
+# Add Title
+st.title("Use Pygwalker In Streamlit")
+ 
+# Import your data
+df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+ 
+# Generate the HTML using Pygwalker
+pyg_html = pyg.walk(df, return_html=True)
+ 
+# Embed the HTML into the Streamlit app
+components.html(pyg_html, height=1000, scrolling=True)
