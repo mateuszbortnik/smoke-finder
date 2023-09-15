be78b6e044e94e1a4e776b5be8db75bdd59da11a
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-15 14:11:27 +0200

    Update Content Analysis.py

--- pages/Content Analysis.py
+++ pages/Content Analysis.py
@@ -25,3 +25,26 @@ else:
 
     # Wait a few seconds before checking task status
 time.sleep(2)
+
+
+    # EXTRACT
+def extract_product_details_from_response(response):
+    all_products = []
+
+    # Directly accessing the location of results based on the structure of your response
+    items = response["tasks"][0]["result"][0]["items"]
+
+    for item in items:
+        product_info = {
+            "url": item["url"],
+            "fetch_time": item["fetch_time"],
+            "country": item["country"],
+            "score": item["score"]
+        }
+        all_products.append(product_info)
+
+    return all_products
+
+    # Usage
+products = extract_product_details_from_response(response)
+print(products)  # This should print the details of the first product
