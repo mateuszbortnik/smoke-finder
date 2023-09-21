a6717ac48d36d02023ada44e91ab6b0728ad614a
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-21 13:52:37 +0200

    Update Google.py

--- pages/Google.py
+++ pages/Google.py
@@ -4,13 +4,56 @@ from client import RestClient
 import pandas as pd
 import time
 import re
+from google.oauth2 import service_account
+from gsheetsdb import connect
+import gspread
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+def save_to_new_worksheet(df, sheet_url, worksheet_name):
+    # Connect to Google Sheets
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    conn = connect(credentials=credentials)
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    
+    # Open the Google Sheet
+    sheet_id = sheet_url.split('/')[-2]
+    sh = gc.open_by_key(sheet_id)
+    
+    # Create a new worksheet with the given name
+    worksheet = sh.add_worksheet(title=worksheet_name, rows="1000", cols="50")
+    
+    # Clear existing data if any (should be empty since it's a new worksheet)
+    worksheet.clear()
+    
+    # Add new data
+    worksheet.insert_rows(df.values.tolist(), row=1)
+    
+    # Add header
+    worksheet.insert_row(df.columns.tolist(), index=1)
+    
+    st.success(f"Data successfully saved to a new worksheet named '{worksheet_name}' in the Google Sheet.")
+
 
 st.title("Google reviews")
 
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
 
-url = st.text_input('url', 'https://www.google.com/maps/place/Ashley+Stewart/@39.8495135,-95.0893464,6z/data=!4m10!1m2!2m1!1sashley+stewart!3m6!1s0x886b523a1ab8c599:0x61c453a9079053a5!8m2!3d39.8495135!4d-86.1245027!15sCg5hc2hsZXkgc3Rld2FydCIDiAEBWhAiDmFzaGxleSBzdGV3YXJ0kgEVd29tZW5zX2Nsb3RoaW5nX3N0b3Jl4AEA!16s%2Fg%2F1txfpx89?authuser=0&entry=ttu')
+url = st.text_input('Google Maps url', 'https://www.google.com/maps/place/Ashley+Stewart/@39.8495135,-95.0893464,6z/data=!4m10!1m2!2m1!1sashley+stewart!3m6!1s0x886b523a1ab8c599:0x61c453a9079053a5!8m2!3d39.8495135!4d-86.1245027!15sCg5hc2hsZXkgc3Rld2FydCIDiAEBWhAiDmFzaGxleSBzdGV3YXJ0kgEVd29tZW5zX2Nsb3RoaW5nX3N0b3Jl4AEA!16s%2Fg%2F1txfpx89?authuser=0&entry=ttu')
+sheet_url = st.text_input('Sheet url', "https://docs.google.com/spreadsheets/d/1pe-M1yQ4jPP8jlH7Hadw1Xkc9KZo2PRTKwaYTnrKxsI/edit#gid=0")
+new_worksheet_name = st.text_input("New worksheet name", "Google reviews")
 
 def extract_cid_from_url(url):
     # Split the URL by "0x"
@@ -114,3 +157,5 @@ if st.button('Get data'):
         mime="text/csv",
         key='download-csv'
     )
+
+    save_to_new_worksheet(df, sheet_url, new_worksheet_name)
