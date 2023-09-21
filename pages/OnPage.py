e9ebf8016cb92a7324d9e1893b0450f0672b865e
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-21 14:47:26 +0200

    Update OnPage.py

--- pages/OnPage.py
+++ pages/OnPage.py
@@ -4,6 +4,46 @@ from client import RestClient
 import pandas as pd
 import time
 from random import Random
+from google.oauth2 import service_account
+from gsheetsdb import connect
+import gspread
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+def save_to_new_worksheet(df, sheet_url, worksheet_name):
+    # Connect to Google Sheets
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    conn = connect(credentials=credentials)
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    
+    # Open the Google Sheet
+    sheet_id = sheet_url.split('/')[-2]
+    sh = gc.open_by_key(sheet_id)
+    
+    # Create a new worksheet with the given name
+    worksheet = sh.add_worksheet(title=worksheet_name, rows="1000", cols="50")
+    
+    # Clear existing data if any (should be empty since it's a new worksheet)
+    worksheet.clear()
+    
+    # Add new data
+    worksheet.insert_rows(df.values.tolist(), row=1)
+    
+    # Add header
+    worksheet.insert_row(df.columns.tolist(), index=1)
+    
+    st.success(f"Data successfully saved to a new worksheet named '{worksheet_name}' in the Google Sheet.")
 
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
@@ -12,6 +52,10 @@ st.text("")
 
 target = st.text_input('Target', 'ashleystewart.com')
 max_crawl_pages = st.text_input('Max crawl pages', '10')
+sheet_url = st.text_input('Sheet url', "https://docs.google.com/spreadsheets/d/1pe-M1yQ4jPP8jlH7Hadw1Xkc9KZo2PRTKwaYTnrKxsI/edit#gid=0")
+new_worksheet_name = st.text_input("New worksheet name", "OnPage data")
+
+
 if st.button('Get data'):
     rnd = Random()
     post_data = dict()
@@ -102,11 +146,11 @@ if st.button('Get data'):
     st.download_button(
         label="Press to Download",
         data=csv,
-        file_name="trustpilot-reviews.csv",
+        file_name="OnPage data.csv",
         mime="text/csv",
         key='download-csv'
     )
-
+    save_to_new_worksheet(df, sheet_url, new_worksheet_name)
 
 
 
