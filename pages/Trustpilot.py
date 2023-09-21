0b21c373c3a8ef8dcdf6540925a816776483e355
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-21 11:49:00 +0200

    Save to new worksheet

--- pages/Trustpilot.py
+++ pages/Trustpilot.py
@@ -24,7 +24,7 @@ def run_query(query):
 sheet_url = "https://docs.google.com/spreadsheets/d/1pe-M1yQ4jPP8jlH7Hadw1Xkc9KZo2PRTKwaYTnrKxsI/edit#gid=0"
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-def save_to_google_sheet(df, sheet_url, worksheet_name='Sheet1'):
+def save_to_new_worksheet(df, sheet_url, worksheet_name):
     # Connect to Google Sheets
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
@@ -34,13 +34,15 @@ def save_to_google_sheet(df, sheet_url, worksheet_name='Sheet1'):
     )
     conn = connect(credentials=credentials)
     gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
-
+    
     # Open the Google Sheet
     sheet_id = sheet_url.split('/')[-2]
     sh = gc.open_by_key(sheet_id)
-    worksheet = sh.worksheet(worksheet_name)
     
-    # Clear existing data
+    # Create a new worksheet with the given name
+    worksheet = sh.add_worksheet(title=worksheet_name, rows="1000", cols="50")
+    
+    # Clear existing data if any (should be empty since it's a new worksheet)
     worksheet.clear()
     
     # Add new data
@@ -49,7 +51,7 @@ def save_to_google_sheet(df, sheet_url, worksheet_name='Sheet1'):
     # Add header
     worksheet.insert_row(df.columns.tolist(), index=1)
     
-    st.success(f"Data successfully saved to {worksheet_name} in the Google Sheet.")
+    st.success(f"Data successfully saved to a new worksheet named '{worksheet_name}' in the Google Sheet.")
 
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
@@ -138,7 +140,9 @@ if st.button('Get data'):
     )
 
     # if st.button('Save to Google Sheets'):
-    #     # Save to Google Sheet
-    save_to_google_sheet(df, sheet_url)
+    # Save to Google Sheet
+    # save_to_google_sheet(df, sheet_url)
         # st.success("Data saved to Google Sheets")
-
+    # Save to a new Google Sheet tab
+    new_worksheet_name = "MyNewWorksheet"
+    save_to_new_worksheet(df, sheet_url, new_worksheet_name)
