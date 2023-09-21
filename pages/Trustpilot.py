7a8a4cc95484fa14bbfa9e5defa7c1cce108114d
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-09-21 10:54:26 +0200

    Update Trustpilot.py

--- pages/Trustpilot.py
+++ pages/Trustpilot.py
@@ -3,6 +3,24 @@ import client
 from client import RestClient
 import pandas as pd
 import time
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = "https://docs.google.com/spreadsheets/d/1pe-M1yQ4jPP8jlH7Hadw1Xkc9KZo2PRTKwaYTnrKxsI/edit#gid=0"
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
