553ca72640f32119f3678b51087f37aa5388feb1
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-10-02 19:55:30 +0200

    Update Google.py

--- pages/Google.py
+++ pages/Google.py
@@ -76,52 +76,56 @@ else:
 
 
 if st.button('Get data'):
-    post_data = dict()
-    # simple way to set a task
-    post_data[len(post_data)] = dict(
-        cid=cid,
-        depth=100,
-        language_name="English",
-        location_name="United States"
-    )
+    with st.status("Sending a POST request...") as status:
 
-    response = client.post("/v3/business_data/google/reviews/task_post", post_data)
+        post_data = dict()
+        # simple way to set a task
+        post_data[len(post_data)] = dict(
+            cid=cid,
+            depth=100,
+            language_name="English",
+            location_name="United States"
+        )
 
-    if response["status_code"] == 20000:
-        # st.write("POST response:", response)
-        task_id = response["tasks"][0]["id"]
-        print("Task ID:", task_id)
-    else:
-        print(f"POST error. Code: {response['status_code']} Message: {response['status_message']}")
-        st.stop()
+        response = client.post("/v3/business_data/google/reviews/task_post", post_data)
 
-        # Wait a few seconds before checking task status
-    time.sleep(2)
-
-    # GET request to fetch the results of the task
-    MAX_RETRIES = 10
-    WAIT_TIME = 10
-    retry_count = 0
-    task_ready = False
-
-    while retry_count < MAX_RETRIES and not task_ready:
-        # st.write(f"Retry count: {retry_count}")  # Debugging line
-        response = client.get(f"/v3/business_data/google/reviews/task_get/{task_id}")
-        # st.write("GET response:", response)
-
-        if response['status_code'] == 20000:
-            task_status = response['tasks'][0]['status_message']
-            # st.write(f"Task status: {task_status}")  # Debugging line
-            if task_status == "Task In Queue":
-                # st.write(f"Attempt {retry_count + 1}: Task is still in queue. Retrying in {WAIT_TIME} seconds...")
-                retry_count += 1
-                time.sleep(WAIT_TIME)
-            elif task_status == "Ok.":  # Only set task_ready = True when the task is actually complete
-                task_ready = True
-                # st.write("Task is ready.")  # Debugging line
+        if response["status_code"] == 20000:
+            # st.write("POST response:", response)
+            task_id = response["tasks"][0]["id"]
+            print("Task ID:", task_id)
         else:
-            st.write(f"GET error. Code: {response['status_code']} Message: {response['status_message']}")
-            break
+            print(f"POST error. Code: {response['status_code']} Message: {response['status_message']}")
+            st.stop()
+
+            # Wait a few seconds before checking task status
+        time.sleep(2)
+
+    with st.status("Waiting for the task") as status:
+        # GET request to fetch the results of the task
+        MAX_RETRIES = 10
+        WAIT_TIME = 10
+        retry_count = 0
+        task_ready = False
+
+        while retry_count < MAX_RETRIES and not task_ready:
+            # st.write(f"Retry count: {retry_count}")  # Debugging line
+            response = client.get(f"/v3/business_data/google/reviews/task_get/{task_id}")
+            # st.write("GET response:", response)
+
+            if response['status_code'] == 20000:
+                task_status = response['tasks'][0]['status_message']
+                # st.write(f"Task status: {task_status}")  # Debugging line
+                if task_status == "Task In Queue":
+                    # st.write(f"Attempt {retry_count + 1}: Task is still in queue. Retrying in {WAIT_TIME} seconds...")
+                    retry_count += 1
+                    time.sleep(WAIT_TIME)
+                elif task_status == "Ok.":  # Only set task_ready = True when the task is actually complete
+                    task_ready = True
+                    status.update(label="Task ready!", state="complete")
+                    # st.write("Task is ready.")  # Debugging line
+            else:
+                st.write(f"GET error. Code: {response['status_code']} Message: {response['status_message']}")
+                break
 
 
     # EXTRACT
