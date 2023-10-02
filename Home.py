3beb31606825e61168eccc259566bc138c51a959
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-10-02 19:33:26 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,12 @@ import time
 client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
 st.title("Smoke Finder💨")
-st.markdown("1. In order to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com")
+txt = '''1. If You want to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com
+2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to 'Summary' tab, the original name will be used.
+
+
+'''
+st.markdown(txt)
 
 
 
