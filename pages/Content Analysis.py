0934dcb9218ad98478b25984d7457b1232db553c
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-10-02 19:49:02 +0200

    Update Content Analysis.py

--- pages/Content Analysis.py
+++ pages/Content Analysis.py
@@ -82,47 +82,47 @@ if st.button('Get data'):
 
             # Wait a few seconds before checking task status
         time.sleep(2)
+    with st.status("Extracting data...") as status:
 
+            # EXTRACT
+        def extract_product_details_from_response(response):
+            all_products = []
 
-        # EXTRACT
-    def extract_product_details_from_response(response):
-        all_products = []
+            # Directly accessing the location of results based on the structure of your response
+            items = response["tasks"][0]["result"][0]["items"]
 
-        # Directly accessing the location of results based on the structure of your response
-        items = response["tasks"][0]["result"][0]["items"]
+            for item in items:
+                product_info = {
+                    "url": item["url"],
+                    "fetch_time": item["fetch_time"],
+                    "country": item["country"],
+                    "score": item["score"],
+                    "content_type": item["content_info"]["content_type"],
+                    "title": item["content_info"]["title"],
+                    "snippet": item["content_info"]["snippet"],
+                    "anger": item["content_info"]["sentiment_connotations"]["anger"],
+                    "happiness": item["content_info"]["sentiment_connotations"]["happiness"],
+                    "love": item["content_info"]["sentiment_connotations"]["love"],
+                    "sadness": item["content_info"]["sentiment_connotations"]["sadness"],
+                    "share": item["content_info"]["sentiment_connotations"]["share"],
+                    "fun": item["content_info"]["sentiment_connotations"]["fun"],
+                    "positive": item["content_info"]["connotation_types"]["positive"],
+                    "negative": item["content_info"]["connotation_types"]["negative"],
+                    "neutral": item["content_info"]["connotation_types"]["neutral"],
+                    "date_published": item["content_info"]["date_published"],
+                    "content_quality_score": item["content_info"]["content_quality_score"]
+                }
+                all_products.append(product_info)
 
-        for item in items:
-            product_info = {
-                "url": item["url"],
-                "fetch_time": item["fetch_time"],
-                "country": item["country"],
-                "score": item["score"],
-                "content_type": item["content_info"]["content_type"],
-                "title": item["content_info"]["title"],
-                "snippet": item["content_info"]["snippet"],
-                "anger": item["content_info"]["sentiment_connotations"]["anger"],
-                "happiness": item["content_info"]["sentiment_connotations"]["happiness"],
-                "love": item["content_info"]["sentiment_connotations"]["love"],
-                "sadness": item["content_info"]["sentiment_connotations"]["sadness"],
-                "share": item["content_info"]["sentiment_connotations"]["share"],
-                "fun": item["content_info"]["sentiment_connotations"]["fun"],
-                "positive": item["content_info"]["connotation_types"]["positive"],
-                "negative": item["content_info"]["connotation_types"]["negative"],
-                "neutral": item["content_info"]["connotation_types"]["neutral"],
-                "date_published": item["content_info"]["date_published"],
-                "content_quality_score": item["content_info"]["content_quality_score"]
-            }
-            all_products.append(product_info)
+            return all_products
 
-        return all_products
-
-    # Usage
-    products = extract_product_details_from_response(response)
-    print(products)  # This should print the details of the first product
-    df = pd.DataFrame.from_dict(products)
-    st.dataframe(df)
-    csv = df.to_csv(index=False)  # Convert the dataframe to CSV string format
-   
+        # Usage
+        products = extract_product_details_from_response(response)
+        print(products)  # This should print the details of the first product
+        df = pd.DataFrame.from_dict(products)
+        st.dataframe(df)
+        csv = df.to_csv(index=False)  # Convert the dataframe to CSV string format
+        status.update(label="Data extracted!", state="complete")
 
     st.download_button(
         label="Press to Download",
