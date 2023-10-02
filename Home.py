e0b791feed5c18eda4aa72ff00c1eb4cce757218
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-10-02 19:36:51 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,8 @@ client = RestClient("marketing@mta.digital", "92626ed1261a7edf")
 
 st.title("Smoke Finder💨")
 txt = '''1. If You want to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com
-2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to 'Summary' tab, the original name will be used.
+2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to the 'Summary' tab, the original name will be used.
+3. When trying to save data to a new worksheet, make sure the worksheet name is unique.
 
 
 '''
