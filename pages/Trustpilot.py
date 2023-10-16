13f4b9913a9e7e128468d7fadb16807f950a4c1e
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-10-16 18:30:56 +0200

    Update Trustpilot.py

--- pages/Trustpilot.py
+++ pages/Trustpilot.py
@@ -69,7 +69,7 @@ if st.button('Get data'):
         response = client.post("/v3/business_data/trustpilot/reviews/task_post", post_data)
 
         if response["status_code"] == 20000:
-            # st.write("POST response:", response)
+            st.write("POST response:", response)
             task_id = response["tasks"][0]["id"]
             # st.write("Task ID:", task_id)
         else:
@@ -80,22 +80,22 @@ if st.button('Get data'):
     time.sleep(2)
     with st.status("Waiting for the task") as status:
         # GET request to fetch the results of the task
-        MAX_RETRIES = 10
+
         WAIT_TIME = 10
         retry_count = 0
         task_ready = False
 
-        while retry_count < MAX_RETRIES and not task_ready:
+        while not task_ready:
             # st.write(f"Retry count: {retry_count}")  # Debugging line
             response = client.get(f"/v3/business_data/trustpilot/reviews/task_get/{task_id}")
-            # st.write("GET response:", response)
+            st.write("GET response:", response)
 
             if response['status_code'] == 20000:
                 task_status = response['tasks'][0]['status_message']
                 # st.write(f"Task status: {task_status}")  # Debugging line
                 if task_status == "Task In Queue":
                     # st.write(f"Attempt {retry_count + 1}: Task is still in queue. Retrying in {WAIT_TIME} seconds...")
-                    retry_count += 1
+
                     time.sleep(WAIT_TIME)
                 elif task_status == "Ok.":  # Only set task_ready = True when the task is actually complete
                     task_ready = True
