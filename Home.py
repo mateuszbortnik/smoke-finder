1d021312622638d31db7394ccc40e332ceaa6a4e
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2024-03-22 15:51:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,51 @@ import client
 from client import RestClient
 import pandas as pd
 import time
+import streamlit_authenticator as stauth
 
 
+username = st.secrets['auth_username ']
+email = st.secrets['email']
+name = st.secrets['auth_name']
+password = st.secrets['auth_password']
+
+# Creating the credentials dictionary
+credentials = {
+    'usernames': {
+        username: {
+            'email': email,
+            'name': name,
+            'password': password,
+            # 'logged_in' is not included as it will be managed automatically
+        }
+    }
+}
+
+# Your configuration for cookie and preauthorized users (replace with your actual values)
+cookie_config = {
+    'expiry_days': 30,
+    'key': 'some_signature_key',  # Replace with your actual key
+    'name': 'some_cookie_name'  # Replace with your actual cookie name
+}
+
+# Instantiate the authenticator
+authenticator = stauth.Authenticate(
+    credentials=credentials,
+    cookie_name=cookie_config['name'],
+    key=cookie_config['key'],
+    cookie_expiry_days=cookie_config['expiry_days'],
+    # Include preauthorized or validator if necessary
+)
+
+authenticator.login()
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 st.title("Smoke Finder💨")
 txt = '''1. If You want to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com
 2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to the 'Summary' tab, the original name will be used.
