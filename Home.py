d02bf962cb1d7f6d30bcceb36bcae9c34b8448d5
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2024-03-22 15:56:08 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -44,25 +44,27 @@ if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
+
+    st.title("Smoke Finder💨")
+    txt = '''1. If You want to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com
+    2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to the 'Summary' tab, the original name will be used.
+    3. When trying to save data to a new worksheet, make sure the worksheet name is unique.
+    4. If something doesn't work, :sob::sob::sob:
+    let him
+    :point_right::point_right::point_right: mateusz.bortnik@mta.digital :point_left::point_left::point_left:
+    know immediately.
+    '''
+    st.markdown(txt)
+
+    st.header("Roadmap")
+    st.markdown("1. Retreive reviews data from Yelp, Trustpilot, Google, Tripadvisor and Yelp and save them to Google Sheets")
+    st.markdown(":green[Done]")
+    st.markdown("2. Reconstruct reports similar to those in Ahrefs/Semrush")
+    st.markdown(":yellow[In progress]")
+    st.markdown("3. Build a summary page that will visualize the data from all sources and draw insights with GPT model")
+    st.markdown(":yellow[In progress]")
+
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
-st.title("Smoke Finder💨")
-txt = '''1. If You want to save data to Google Sheets, create a new spreadsheet  and share it with the service account email address: streamlit@mta-digital-bi.iam.gserviceaccount.com
-2. You can change the suggested 'New worksheet name', but when fetching data back from sheets to the 'Summary' tab, the original name will be used.
-3. When trying to save data to a new worksheet, make sure the worksheet name is unique.
-4. If something doesn't work, :sob::sob::sob:
-let him
-:point_right::point_right::point_right: mateusz.bortnik@mta.digital :point_left::point_left::point_left:
-know immediately.
-'''
-st.markdown(txt)
-
-st.header("Roadmap")
-st.markdown("1. Retreive reviews data from Yelp, Trustpilot, Google, Tripadvisor and Yelp and save them to Google Sheets")
-st.markdown(":green[Done]")
-st.markdown("2. Reconstruct reports similar to those in Ahrefs/Semrush")
-st.markdown(":yellow[In progress]")
-st.markdown("3. Build a summary page that will visualize the data from all sources and draw insights with GPT model")
-st.markdown(":yellow[In progress]")
